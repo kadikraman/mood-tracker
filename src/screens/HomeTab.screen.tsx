442cf524f2c41a6f839875d2cf6d9b32ee5cb138
Kadi Kraman <hellokadi@gmail.com>
2021-05-18 09:40:49 +0100

    Add local image

--- src/screens/HomeTab.screen.tsx
+++ src/screens/HomeTab.screen.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
 import { useAppContext } from '~src/App.provider';
 import { MoodPicker } from '~src/components/MoodPicker';
+
+const imageSrc = require('~src/assets/images/ying-yang.png');
 
 export const HomeTab = () => {
   const { handleAddMood } = useAppContext();
 
   return (
     <View style={styles.container}>
+      <Image source={imageSrc} style={styles.image} />
       <MoodPicker onAddMood={handleAddMood} />
     </View>
   );
@@ -16,6 +19,9 @@ export const HomeTab = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+  },
+  image: {
+    alignSelf: 'center',
+    marginVertical: 20,
   },
 });
