59454ef0cca111ca6b475e0cb49326e5ae6ce242
Kadi Kraman <hellokadi@gmail.com>
2021-05-18 18:00:54 +0100

    Configure react-native-splash-screen for iOS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Platform, UIManager } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './screens/Root.navigator';
 import { AppProvider } from './App.provider';
@@ -11,6 +12,10 @@ if (Platform.OS === 'android') {
 }
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <AppProvider>
       <NavigationContainer>
