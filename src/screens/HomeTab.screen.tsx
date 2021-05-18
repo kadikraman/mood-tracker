07908e87de8a1951e0eec12c9d676e4c8463eed7
Kadi Kraman <hellokadi@gmail.com>
2021-05-18 09:56:31 +0100

    Use ImageBackground for the background image

--- src/screens/HomeTab.screen.tsx
+++ src/screens/HomeTab.screen.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { StyleSheet, Image, ImageBackground } from 'react-native';
 import { useAppContext } from '~src/App.provider';
 import { MoodPicker } from '~src/components/MoodPicker';
 
 const imageSrc = require('~src/assets/images/ying-yang.png');
+const networkImageUrl =
+  'https://images.unsplash.com/photo-1621193677201-096db5e45734?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80';
 
 export const HomeTab = () => {
   const { handleAddMood } = useAppContext();
 
   return (
-    <View style={styles.container}>
+    <ImageBackground style={styles.container} source={{ uri: networkImageUrl }}>
       <Image source={imageSrc} style={styles.image} />
       <MoodPicker onAddMood={handleAddMood} />
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -22,6 +24,7 @@ const styles = StyleSheet.create({
   },
   image: {
     alignSelf: 'center',
-    marginVertical: 20,
+    marginTop: 50,
+    marginBottom: 20,
   },
 });
